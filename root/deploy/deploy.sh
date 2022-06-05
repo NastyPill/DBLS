0b8a5a7f963c05143a0c4a75e3a620127016e461
valeriyTanashkin <Djponnyash.vinyl@gmail.com>
2022-06-06 01:57:53 +0300

    complete local deploy

--- root/deploy/deploy.sh
+++ root/deploy/deploy.sh
@@ -2,18 +2,24 @@
 
 cd ../app/docker/server-node
 docker build -t server .
-cd ../postgresql
-docker build -t postgres .
+
 cd ../../../client/docker/client-node
 docker build -t client .
 cd ../rmq
 docker build -t rmq .
+cd ../../../rest/docker/rest
+docker build -t rest .
 
-docker create --net="host" --name="rmq" rmq
-docker create --net="host" --name="postgres" postgres
-docker create --net="host" --name="dataProcessor" -e "PORT=2551" -e "ROLE=dataProcessor" server
-docker create --net="host" --name="listener1" -e "PORT=2555" -e "ROLE=listener" server
-docker create --net="host" --name="listener2" -e "PORT=2556" -e "ROLE=listener" server
-docker create --net="host" --name="balancer" -e "PORT=3551" -e "ROLE=balancer" client
-docker create --net="host" --name="searcher1" -e "PORT=3555" -e "ROLE=searcher" client
-docker create --net="host" --name="searcher2" -e "PORT=3556" -e "ROLE=searcher" client
+docker run -d --net="host" --name="rmq" rmq
+docker run -d --net="host" --name="dataProcessor" -e "PORT=2551" -e "ROLE=dataProcessor" server
+docker run -d --net="host" --name="listener1" -e "PORT=2555" -e "ROLE=listener" server
+docker run -d --net="host" --name="listener2" -e "PORT=2556" -e "ROLE=listener" server
+sleep 5
+docker restart rmq
+sleep 10
+docker run -d --net="host" --name="balancer" -e "PORT=3551" -e "ROLE=balancer" client
+docker run -d --net="host" --name="searcher1" -e "PORT=3555" -e "ROLE=searcher" client
+docker run -d --net="host" --name="searcher2" -e "PORT=3556" -e "ROLE=searcher" client
+docker run -d --net="host" --name="rest" rest
+
+
